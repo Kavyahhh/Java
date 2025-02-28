bbe763de09dd841188c6b2ec82cb40469260e1d7
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-02-28 14:24:27 +0530

    Update Pattern3.java

--- Week-2/Pattern3.java
+++ Week-2/Pattern3.java
@@ -7,7 +7,7 @@ class Main{
             for(int j=1; j<=i; j++){
                 System.out.print(j + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
