94894e1abee7879a5a338c4978626335d2165d0d
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-02-28 14:23:13 +0530

    Update Pattern1.java

--- Week-2/Pattern1.java
+++ Week-2/Pattern1.java
@@ -7,7 +7,7 @@ public class Main{
             for(int j=1;j<=n;j++){
                 System.out.print("*"+" ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
