eee515c2caf4e88b8a617061ddf12c7a2782b7b0
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-03-03 14:27:13 +0530

    Update Pattern14.java

--- Week-2/Patterns/Pattern14.java
+++ Week-2/Patterns/Pattern14.java
@@ -1,3 +1,27 @@
+/*Sample 1:
+Input:
+n = 8
+Expected output:
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+6 6 6 6 6 6 
+7 7 7 7 7 7 7 
+8 8 8 8 8 8 8 8 
+
+Sample 2
+Input:
+n= 6
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+6 6 6 6 6 6 
+Hint: Check pattern 13 and think in terms of using count.*/
+
 import java.util.Scanner;
 class Main{
     public static void main(String[] args){
