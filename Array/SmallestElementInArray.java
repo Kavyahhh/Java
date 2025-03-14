422ec089f7482e0bc20e107162908c6b6a0c162f
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-03-14 14:04:28 +0530

    Update SmallestElementInArray.java

--- Array/SmallestElementInArray.java
+++ Array/SmallestElementInArray.java
@@ -12,3 +12,26 @@ class SmallestElement{
         System.out.print(Smallest);
     }
 }
+
+//solution-2
+import java.util.Scanner;
+class SmallestElement{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i;
+        int [] a = new int[n];
+        for( i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            }
+        int Smallest = a[0];
+        for( i=0;i<n;i++)
+        {
+            if(Smallest>a[i]){
+                Smallest=a[i];
+            }
+            
+        }
+      System.out.print(Smallest);
+    }
+}
