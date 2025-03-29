6972ecf8f7b8da79ee83f73e91c1d72f4178631a
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-03-29 10:59:23 +0530

    Create LongestCommonPrefix.java

---
+++ String/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String first=strs[0];
+        String last=strs[strs.length-1];
+        int index=0;
+        while(index<first.length()){
+            if(first.charAt(index)==last.charAt(index)){
+                index++;
+            }
+            else{
+                break;
+            }
+        }
+        if(index==0){
+            return "";
+        }
+        else{
+            return first.substring(0,index);
+        }
+    }
+}
