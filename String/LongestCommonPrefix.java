30cd4597d2dd46823fc9607a53c98c26cc53e89d
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-03-29 11:35:15 +0530

    Update LongestCommonPrefix.java

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/longest-common-prefix/
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
         String first=strs[0];
         String last=strs[strs.length-1];
         int index=0;
